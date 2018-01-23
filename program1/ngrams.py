d100fd5b8b2742d04f780a6dd861b81c1321fdbb
xflattopx <xflattopx@gmail.com>
2018-01-23 16:54:03 -0500

    Add files via upload
    Chase's version of writing to an output file.

--- program1/ngrams.py
+++ program1/ngrams.py
@@ -1,21 +1,45 @@
 import re
 
-def ngrams(n, text):
-    newtext = [x for x in text if x.isalpha()]
-    #newtext = [
-    return newtext
+
+#compute_ngram_charfreq(invertedIndex, charFile, n, text) computes the frequency
+#of each word in the given file.
+#invertedIndex is the inverted index
+#charFreq is the frequency in which each character appears.
+#n is the size of grams (number of tokens).
+#text is the text we're working with.
+def compute_ngram_charfreq(invertedIndex, charFreq, n, text):
+	i = 0
+	while i < len(text):
+		if (i + n) < len(text):
+			ngram_as_list = text[i:i+n] 
+			ngram = " ".join(ngram_as_list) #converts ngram to string
+			
+			#adds/inserts ngram to dictonary invertedIndex
+			if ngram in invertedIndex:
+				invertedIndex[ngram] += 1
+			else:
+				invertedIndex[ngram] = 1
+		
+		compute_char_freq(charFreq, text[i])
+		
+		i = i + 1
+
+
+#compute_char_freq(char_freq, word) calculates the character frequency 
+def compute_char_freq(char_freq, token):
+	for i in token:
+		char_freq[rank(i)] += 1
+
+#calculates position of character c in charFreq array
+#rank('a') = 0
+def rank(char):
+	return ord(char) - 97
 
 #returns list of words from file called filename
 def give_word_list(filename):
     f = open(filename, 'r')
     words = [token for line in f for token in line.split()]
     return words
-
-def calc_ngrams_charfreq(invertedIndex, freqArray, text, n):
-    for i in text:
-        text[i] = tokenize(text[i])
-        
-    return
 
 #tokenizes a word
 def tokenize(word):
@@ -24,6 +48,7 @@ def tokenize(word):
     w = w.lower()
     return w
 
+#tokenizes a listof words
 def tokenize_word_list(wordlist):
     i = 0
     while i < len(wordlist):
@@ -31,7 +56,63 @@ def tokenize_word_list(wordlist):
         i = i + 1
     return wordlist
 
-#print tokenize('Cow.')
+#writes character frequency to output file
+#out_file is the file in which we will be writing to.
+def write_to_outfile(out_file):
+    ofile = open(out_file,'w') #writes to the file
+    i = 0
+    while i < len(charFreq):
+		ofile.write(chr(i + 97) + "-" + str(charFreq[i]) + "\n")
+		print(chr(i + 97) + "-" +  str(charFreq[i]) + "\n")
+		i += 1
+		
+    ofile.close()
+	
+
+
+	
+
+
+#####Main######
+
+
+invertedIndex = {}
+
+#charater frequenct array
+#charFreq[0] represents the character a
+#charFreq[1] represents the character b
+#....
+#charFreq[25] represents the character z
+charFreq = [0] * 26
+
+#n is the size of the n-gram 
+n = 5
 
 text1 = give_word_list('austen-emma.txt')
-print tokenize_word_list(text1)
+#text1 = give_word_list('test.txt')
+
+
+text1 = tokenize_word_list(text1)
+
+compute_ngram_charfreq(invertedIndex, charFreq, n, text1)
+
+#invertedIndex = sorted(invertedIndex, key = invertedIndex.value())
+
+#i = len(invertedIndex)
+#while i > 0:
+#    print "%s: %s" % (key, mydict[key])
+
+print charFreq
+
+IItuples = [(value, key) for key, value in invertedIndex.iteritems()]
+SortedTuples = sorted(IItuples, key=lambda x: x[0], reverse=True)
+
+#print IItuples
+#print SortedShit
+
+write_to_outfile('char_freq.txt')
+
+j = 0
+while j < 10:
+	print SortedTuples[j]
+	j += 1
